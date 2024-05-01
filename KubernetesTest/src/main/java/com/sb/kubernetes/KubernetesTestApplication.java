ddcc2fa512fddbb18ad9ab47cf78a36fdf650431
sbhshkmr674 <jietss2011@gmail.com>
2024-05-01 17:44:18 +0530

    1st Commit

---
+++ KubernetesTest/src/main/java/com/sb/kubernetes/KubernetesTestApplication.java
@@ -0,0 +1,14 @@
+package com.sb.kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesTestApplication.class, args);
+		System.out.println("KkubernetesTestApplication.main()");
+	}
+
+}
---
+++ KubernetesTest/src/test/java/com/sb/kubernetes/KubernetesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
